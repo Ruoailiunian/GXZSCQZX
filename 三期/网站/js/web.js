9024bd5104cf9254202765360d4235e81d3f340b
Ruoailiunian <47737419+Ruoailiunian@users.noreply.github.com>
2019-04-24 10:00:16 +0800

    Initial commit

---
+++ 三期/网站/js/web.js
@@ -0,0 +1,70 @@
+;!function(){
+	  var carousel = layui.carousel;
+	  //建造实例
+	  carousel.render({
+	    elem: '#test1'
+	    ,width: '100%' //设置容器宽度
+	    ,arrow: 'always' //始终显示箭头
+	    ,height:"350px"
+	    ,indicator:'none'
+	    //,anim: 'updown' //切换动画方式
+	  });
+}();
+
+
+$(function(){
+	var widW=true;
+	$('.depdiv').mouseover(depdivmouse);
+	$('.fzx').hover(fzxover,fzxout);
+	$('.layui-tab-title>li').click(tabliclick);
+	$('.ulmore>li:first').show();
+	$('.shudh>ul>li:last').click(FhTop);
+	$('.shudh>ul>li').hover(shudhove,shudhout);
+	$(window).resize(windowT)
+	
+
+	function depdivmouse(){
+		if(widW){
+			$(this).addClass('active').siblings().removeClass('active');
+		}
+	}
+
+	function fzxover(){
+		$(this).find('ul').show();
+	}
+
+	function fzxout(){
+		$(this).find('ul').hide();
+	}
+
+	function tabliclick(len){
+		var len=$(this).parent('ul').children('li').index(this);
+		$(this).siblings('ul.ulmore').children('li').eq(len).show().siblings('li').hide();;
+	};
+
+	function FhTop(){
+		$('html , body').animate({scrollTop: 0},'slow');
+	};
+
+	function shudhove(){
+		$(this).find('div').show();
+	};
+
+	function shudhout(){
+		$(this).find('div').hide();
+	};
+
+	function windowT(){
+		var windowWidth=$(window).width();
+		if(windowWidth<992){
+			$('.depdiv').addClass('active');
+			widW=false;
+		}else{
+			$('.depdiv').removeClass('active');
+			$('.depdiv:first').addClass('active')
+			widW=true;
+		}
+	};
+	windowT();
+
+})
---
+++ 二期/云南省技术交易市场/主站/网站/web1css/js/web.js
@@ -0,0 +1,35 @@
+$(function(){
+		//选项卡
+	$('.XxNewBt').each(function(){
+		$(this).children('ul').children('li').eq(0).addClass('active')
+	});
+	$('.XxList').each(function(){
+		$(this).children('ul').eq(0).show();
+	});
+	$('.XxNewBt>ul>li').mouseover(Xxk);
+	function Xxk(){
+		var len=$(this).parent('ul').children('li').index(this);
+		$(this).siblings('li').removeClass('active');
+		$(this).addClass('active');
+		$(this).parents('.XxNewBt').siblings('.XxList').children('ul').hide();
+		$(this).parents('.XxNewBt').siblings('.XxList').children('ul').eq(len).show();
+	}
+
+	//导航条
+	//
+	$('.navN>ul>li').hover(function(){
+		$(this).siblings('li').removeClass('active');
+		$(this).addClass('active')
+		$(this).find('a').addClass('animated flipInX');
+	},function(){
+		$('.navN>ul>li').removeClass('active');
+		$('.navN>ul>li>a').removeClass('animated flipInX');
+		$('.navN>ul>li:first').addClass('active');
+	})
+
+	//站点切换
+	$('#QhList').click(function(){
+		$('.QhListli').slideToggle()
+	})
+
+})
